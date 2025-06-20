abb902c87157752c4f78abd71bf6b9456c89b770
Matúš Jakab <jkb.1327@gmail.com>
2025-06-20 17:28:37 +0200

    fix2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "./api/auth/[...nextauth]/route";
+import { authOptions } from "./api/auth/[...nextauth]/authOptions";
 import HomeClient from "./HomeClient";
 
 export default async function Home() {
